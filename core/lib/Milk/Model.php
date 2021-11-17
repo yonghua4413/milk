4b1ead67decef97631b4ee1276134ac257653947
ttitt <i@ttitt.net>
2021-11-17 10:22:43 +0800

    新增Model类，暂时还未实现功能

---
+++ core/lib/Milk/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of Milk framework
+ *
+ * Author: ttitt <378797263@qq.com>
+ * 
+ * © Milk framework
+ */
+
+namespace Milk;
+
+use Milk\Db\Drive;
+
+class Model
+{
+    public function __construct()
+    {
+    }
+
+    public function allowField($field)
+    {
+    }
+}
