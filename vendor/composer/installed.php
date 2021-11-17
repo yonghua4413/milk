fc5518523794ed266b668719424ea8272c596c83
ttitt <378797263@qq.com>
2021-11-17 09:41:45 +0800

    框架支持composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ff43c0385484fd390297b1b6f75af9e44288e395',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ff43c0385484fd390297b1b6f75af9e44288e395',
+            'dev_requirement' => false,
+        ),
+    ),
+);
